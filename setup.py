a6bd6fcb89e8bad8d601d0f780b8ffd8e8406e59
m0zu <m0zurillex@gmail.com>
2018-12-31 19:16:05 +0900

    upload pypi

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(
     name='dmm_search3',
-    version='0.3',
+    version='1.0',
     description='DMM Web API v3.0 for Python3',
     url='https://github.com/0x0u/dmm',
     author='m0zu',
@@ -13,7 +13,7 @@ setup(
     license='MIT',
     keywords='Python3 dmm API ',
     packages=find_packages(),
-    install_requires=install_requires,
+    install_requires=install_reqs,
     long_description=open('README.md').read(),
     long_description_content_type='text/markdown',
     classifiers=['Programming Language :: Python :: 3.6']
