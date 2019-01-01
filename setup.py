dfc3c165ddb70906a9bb351b57e7cf217ea76908
m0zu <m0zurillex@gmail.com>
2019-01-02 01:11:07 +0900

    update

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
 
 setup(
     name='dmm_search3',
-    version='1.0',
+    version='1.1',
     description='DMM Web API v3.0 for Python3',
     url='https://github.com/0x0u/dmm',
     author='m0zu',
