5014bd3f825d264921cfd3fc5a192b19e2c2404c
m0zu <m0zurillex@gmail.com>
2019-06-05 19:01:21 +0900

    video_downloadメソッドの調整

--- dmm/DMM.py
+++ dmm/DMM.py
@@ -53,21 +53,30 @@ class DMM():
     第二引数はファイル名を指定。第二引数を省略した場合はcontent_idがそのままファイル名になる。
     '''
     @classmethod
-    def sample_download(cls, cid, fname=None):
-        url = 'http://www.dmm.co.jp/litevideo/-/detail/=/cid={}/'.format(cid)
+    def video_download(cls, cid, fname=None):
+        url = 'https://www.dmm.co.jp/litevideo/-/detail/=/cid={}'.format(cid)
         req = requests.get(url)
         status = req.status_code
         if status == 200:
-            m = re.findall('{"content_id":"(.+)"}', req.text)[0]
-            url2 = 'http://cc3001.dmm.co.jp/litevideo/freepv/{}/{}/{}/{}_sm_w.mp4'.format(m[:1], m[:3], m, m)
+            soup = BeautifulSoup(req.text, 'lxml')
+            f = soup.find('iframe', allow="autoplay").get('src')
+            r = re.search(r'cid=(.*)/mtype', f)
+            if r != None:
+                rcid = r.group(1)
+            else:
+                return 404
+            # 320 × 180
+            url2 = 'http://cc3001.dmm.co.jp/litevideo/freepv/{}/{}/{}/{}_sm_w.mp4'.format(rcid[:1], rcid[:3], rcid, rcid)
+            # 720 × 404
+            # url2 = 'http://cc3001.dmm.co.jp/litevideo/freepv/{}/{}/{}/{}_dmb_w.mp4'.format(rcid[:1], rcid[:3], rcid, rcid)
             req2 = requests.get(url2)
             status2 = req2.status_code
             if status2 == 200:
                 if fname == None:
                     fname = cid
-                ydl_opts = {
-                    'outtmpl':fname + '.mp4',
-                    'quiet':True
+                    ydl_opts = {
+                        'outtmpl':fname + '.mp4',
+                        'quiet':True
                     }
                 with youtube_dl.YoutubeDL(ydl_opts) as ydl:
                     ydl.download([url2])
