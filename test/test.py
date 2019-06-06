7e5bb3f151fe2cb88cc9699491cee13c963c35e7
m0zu <m0zurillex@gmail.com>
2019-06-06 09:50:08 +0900

    テストコードの編集

--- test/test.py
+++ test/test.py
@@ -7,32 +7,29 @@ from dmm import DMM
 api_id = os.environ.get('api_id')
 affiliate_id = os.environ.get('affiliate_id')
 
-
-#インスタンスを作成
+# インスタンスを作成
 dmm = DMM(api_id=api_id, affiliate_id=affiliate_id)
 
-#検索メソッド
-
-#商品検索API
+# 商品検索API
 item = dmm.search('ItemList', keyword='明日花キララ')
 
-#フロアAPI
+# フロアAPI
 floor = dmm.search('floorList')
 
-#女優検索API
+# 女優検索API
 actress = dmm.search('ActressSearch', keyword='明日花キララ')
 
-#ジャンル検索API
+# ジャンル検索API
 genre = dmm.search('GenreSearch', keyword='明日花キララ')
 
-#メーカー検索API
+# メーカー検索API
 maker = dmm.search('MakerSearch', initial='あ')
 
-#シリーズ検索API
+# シリーズ検索API
 series = dmm.search('SeriesSearch', initial='あ')
 
-#作者検索API
+# 作者検索API
 author = dmm.search('AuthorSearch', initial='あ')
 
-#サンプル動画ダウンロードメソッド
+# サンプル動画ダウンロードメソッド
 DMM.video_download('ssni378')
