7920ed85711d8441e78af45aa6c9064fa16bd3f2
0x0u <m0zurillex@gmail.com>
2020-04-04 12:20:11 +0900

    Version 2.1 Release🎍

--- setup.py
+++ setup.py
@@ -1,20 +1,19 @@
-import os
 from setuptools import setup, find_packages
 
-install_reqs = [line.strip() for line in open('requirements.txt').readlines()]
+install_reqs = [line.strip() for line in open("requirements.txt").readlines()]
 
 setup(
-    name='dmm_search3',
-    version='2.0',
-    description='DMM Web API v3.0 for Python3',
-    url='https://github.com/0x0u/dmm_search3',
-    author='m0zu',
-    author_email='m0zurillex@gmail.com',
-    license='MIT',
-    keywords='Python3 dmm API ',
+    name="dmm_search3",
+    version="2.1",
+    description="DMM Web API Version 3.0 Wrapper for Python3",
+    url="https://github.com/miya/dmm_search3",
+    author="miya",
+    author_email="m0zurillex@gmail.com",
+    license="MIT",
+    keywords="Python3 dmm API",
     packages=find_packages(),
     install_requires=install_reqs,
-    long_description=open('README.md').read(),
-    long_description_content_type='text/markdown',
-    classifiers=['Programming Language :: Python :: 3.6']
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    classifiers=["Programming Language :: Python :: 3.7"]
 )
