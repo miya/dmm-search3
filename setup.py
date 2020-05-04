340ada79ebcd5d109d4cd2d286451ba5303ccf92
0x0u <m0zurillex@gmail.com>
2020-05-04 22:56:55 +0900

    Version 2.3 Release 🚀

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ install_requires = [line.strip() for line in open("requirements.txt").readlines(
 
 setup(
     name="dmm-search3",
-    version="2.2",
+    version="2.3",
     description="DMM Web API Version 3.0 Wrapper for Python3",
     url="https://github.com/miya/dmm-search3",
     author="miya",
